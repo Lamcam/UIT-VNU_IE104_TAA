962935b0261c9773e94513e3a507042689a5dc6a
anhuynh219 <anhuynh200319@gmail.com>
2023-10-24 13:12:12 +0700

    update Js jquery accountPage

---
+++ TAA_FE/src/homepage/accountPage/accountPage.js
@@ -0,0 +1,37 @@
+const inputArr = [
+    "add-password",
+    "add-name",
+    "add-phonenumber",
+    "add-email",
+  ];
+
+  setEveryInputReadOnly();
+
+  function setEveryInputReadOnly() {
+    inputArr.forEach(function (index) {
+      $("." + index).prop("readonly", true);
+    });
+  }
+
+  function activeInput() {
+    let targetIndex = $(".input-icon").index(this);
+    $("." + inputArr[targetIndex]).prop("readonly", false);
+  }
+
+  $(".input-icon").on("click", activeInput);
+  $(".header__btn--login").on("click", setEveryInputReadOnly);
+
+
+  function addActiveClass(element, activeClassName) {
+      $(element).siblings().removeClass(activeClassName);
+      $(element).addClass(activeClassName);
+  }
+
+  $(".profile-li").on("click", function() {
+     addActiveClass(this, "active-li");
+     
+  });
+
+
+
+
