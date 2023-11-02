a3b873ec24878a702ac9d585ead4d305f66966bc
FredLe09 <le197903@gmail.com>
2023-11-02 15:04:13 +0700

    Update imgs__path, aboutUsPage, 'remove some lines of res/js'

--- TAA_FE/res/js/modal--controller.js
+++ TAA_FE/res/js/modal--controller.js
@@ -1,8 +1,6 @@
 setTimeout(() => {
   const openModalBtns = document.querySelectorAll("[data-modal-target]");
-  console.log(openModalBtns);
   openModalBtns?.forEach(btn => {
-    console.log(btn);
     btn?.addEventListener("click", (event) => {
       const modalId = btn.dataset.modalTarget;
       const modal = document.querySelector(modalId);
