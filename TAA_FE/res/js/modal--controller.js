7b0d2b476b2504dd26b173ca52eeea3c2493464e
FredLe09 <le197903@gmail.com>
2023-11-17 21:41:19 +0700

    Remove cmt in modal--controller.js

--- TAA_FE/res/js/modal--controller.js
+++ TAA_FE/res/js/modal--controller.js
@@ -1,10 +1,10 @@
 $(window).on('load', () => {
   const openModalBtns = document.querySelectorAll("[data-modal-target]");
-  // console.log(openModalBtns);
+  console.log(openModalBtns);
   openModalBtns?.forEach((btn) => {
     btn?.addEventListener("click", (event) => {
       const modalId = btn.dataset.modalTarget;
-      // console.log(modalId);
+      console.log(modalId);
       const modal = document.querySelector(modalId);
       modal?.classList.add("active");
     });
