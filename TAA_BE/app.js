f9a7c37709e0babbc912c9fcf9364d67a98a7c54
FredLe09 <le197903@gmail.com>
2023-11-19 14:30:28 +0700

    Fix BE>db.js+app.js

--- TAA_BE/app.js
+++ TAA_BE/app.js
@@ -16,23 +16,35 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
+
+const __dirnameList = __dirname.split('\\')
+// console.log(__dirnameList.slice(0, -1).join('\\') + "\\TAA_FE\\src\\homepage\\index.html")
+const __rootDir = __dirnameList.slice(0, -1).join('\\')
+// console.log(__rootDir + "\\TAA_FE\\src\\homepage\\index.html")
+const __srcDir = __rootDir + "\\TAA_FE\\src\\homepage"
+// console.log(__srcDir + "\\index.html")
 
 // app.use(express.static(path.join()))
 
 app.get("/", async (req, res) => {
-    // res.send("Hj")
-    res.sendFile(__dirname + "/index.html");
+  // console.log(__dirnameList.slice(0, -1).join('\\') + "\\TAA_FE\\src\\homepage\\index.html")
+  // res.sendFile(__dirname.split('/')[0,-1].join('/') + "/TAA_FE/src/homepage/index.html");
+
+
+  // res.status(200).sendFile(__srcDir + "\\index.html");
+
+
+  res.status(200).sendFile(__dirname + "\\index.html");
 });
 
 app.get("/getAll", async (req, res) => {
   try {
     const db = await DbService.getDbServiceInstance();
     const result = await db.getAllData();
-    
-    // result.then(data => res.json({data: data})).catch(err =>console.log(err));
-    res.json({result});
 
+    // result.then(data => res.json({data: data})).catch(err => console.log(err));
+    res.status(200).json({ result });
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "An error occurred" });
--- TAA_BE/config/db.js
+++ TAA_BE/config/db.js
@@ -1,19 +1,19 @@
-const mysql = require("mysql") ;
+const mysql = require("mysql");
 const dotenv = require('dotenv');
 dotenv.config();
 
-let instance =null ;
+let instance = null;
 
 const connection = mysql.createConnection({
-    connectionLimit:10,
-    host        : 'localhost',
-    user        : 'root',
-    password    : '',
-    database    : 'taa'
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'database_ie104'
 })
 
-connection.connect((err)=>{
-    if(err){
+connection.connect((err) => {
+    if (err) {
         console.log(err.message);
     }
 
@@ -22,32 +22,31 @@ connection.connect((err)=>{
 })
 
 class Dbservice {
-    static getDbServiceInstance(){
+
+    static getDbServiceInstance() {
         return instance ? instance : new Dbservice();
     }
-    async getAllData () {
-        try{
-            const resspone = await new Promise((resolve,reject)=> {
-                const query = "SELECT * FROM customer";
 
-                connection.query(query,(err,results) => {
-                    if(err) reject(new Error(err.message));
-                    resolve(results);
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM users";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    else resolve(results);
                 })
             })
 
-            return resspone;
+            return response;
         }
-        catch(error){
-
+        catch (error) {
             console.log(err);
-
         }
-      }
-    
+    }
 }
 
 
 // module.exports = connectDB;
-module.exports = Dbservice ;
+module.exports = Dbservice;
 
