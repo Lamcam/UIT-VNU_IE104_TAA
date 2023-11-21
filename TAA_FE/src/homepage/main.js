e3c3a606dc38de64fde4726a7e035f0159804a25
FredLe09 <le197903@gmail.com>
2023-11-21 21:43:48 +0700

    Update modal--controller.j+/homepage/main.js+/homepage/index.html

--- TAA_FE/src/homepage/main.js
+++ TAA_FE/src/homepage/main.js
@@ -1,15 +1,10 @@
-// const modal = require('/TAA_FE/res/js/modal--controller.js');
+$(document).ready(function () {
+  $("#header").load("/TAA_FE/src/partials/header--unauth.html");
+  $("#footer").load("/TAA_FE/src/partials/footer.html");
 
-// $('#footer').load('/TAA_FE/src/partials/footer.html');
-
-fetch('/TAA_FE/src/components/product__item.html')
-  .then(res => { console.log(res); return res.text(); })
-  .then(res => {
-    for (let i = 0; i < 9; ++i)
-      $('.product__wrapper').append(res)
-  })
-  .catch(err => console.log(err));
-
-// $(() => $('#main-nav').load('/TAA_FE/src/components/main-nav.html'));
-
-// modal.loadOpenModal();
+  fetch("/TAA_FE/src/components/product__item.html")
+    .then((res) => res.text())
+    .then((res) => {
+      for (let i = 0; i < 9; ++i) $(".product__wrapper").append(res);
+    });
+});
