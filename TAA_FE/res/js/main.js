de73a5114757d0e35cd2902964c5d7266797cca9
FredLe09 <le197903@gmail.com>
2023-11-23 00:06:33 +0700

    Update #modal--login, #modal--register resposive

--- TAA_FE/res/js/main.js
+++ TAA_FE/res/js/main.js
@@ -2,3 +2,8 @@ function closeModal() {
   const modals = document.querySelectorAll(".modal");
   modals.forEach(modal => modal.classList.remove("active"))
 }
+
+const openModal = (sltById) => {
+  const modalSelector = document.querySelector(sltById);
+  modalSelector.classList.add("active");
+}
