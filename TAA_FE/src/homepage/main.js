f672ba447fd71e02fe5eb144bb3948c55664dace
FredLe09 <le197903@gmail.com>
2023-11-23 02:50:47 +0700

    Update new modals into /homepage

--- TAA_FE/src/homepage/main.js
+++ TAA_FE/src/homepage/main.js
@@ -2,7 +2,8 @@ $(() => {
   $("#header").load("/TAA_FE/src/partials/header--unauth.html");
   $("#modal__login").load("/TAA_FE/src/partials/modal--login.html");
   $("#modal__register").load("/TAA_FE/src/partials/modal--register.html");
-  $("#modal__forgot").load("/TAA_FE/src/partials/modal--forgot.html");
+  $("#modal__forgot-method").load("/TAA_FE/src/partials/modal--forgot-method.html");
+  $("#modal__forgot-by-phone").load("/TAA_FE/src/partials/modal--forgot-by-phone.html");
 
   $("#footer").load("/TAA_FE/src/partials/footer.html");
 
