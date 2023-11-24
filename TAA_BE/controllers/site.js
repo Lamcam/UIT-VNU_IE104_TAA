c6901aea146df878d82758c9ad36065d2c85eefc
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2023-11-24 15:08:24 +0700

    Update data for controller/site.js

--- TAA_BE/controllers/site.js
+++ TAA_BE/controllers/site.js
@@ -9,16 +9,20 @@ site.about = (req, res) => {
 }
 
 site.news = (req, res) => {
+  const title = `GIẢI MÃ PHONG CÁCH Y2K`
   const context = `
-    
+    Y2K - phong cách thời trang nổi loạn có lẽ không còn xa lạ gì với chúng ta
+    bởi thời trang và xu hướng thẩm mỹ từ cuối thập niên 90 đầu 2000 - đã gây
+    chú ý trở lại trong những năm gần đây và được các tín đồ thời trang chào
+    đón nồng nhiệt.
   `
   newsDatas = [
-    { imgPath: '', title: 'TAA 2021', context: 'TAA 2021 is coming soon' },
-    { imgPath: '', title: 'TAA 2021', context: 'TAA 2021 is coming soon' },
-    { imgPath: '', title: 'TAA 2021', context: 'TAA 2021 is coming soon' },
-    { imgPath: '', title: 'TAA 2021', context: 'TAA 2021 is coming soon' },
-    { imgPath: '', title: 'TAA 2021', context: 'TAA 2021 is coming soon' },
-    { imgPath: '', title: 'TAA 2021', context: 'TAA 2021 is coming soon' },
+    { imgPath: '', title, context },
+    { imgPath: '', title, context },
+    { imgPath: '', title, context },
+    { imgPath: '', title, context },
+    { imgPath: '', title, context },
+    { imgPath: '', title, context },
   ]
 
   res.render('pages/site/news', { newsDatas })
