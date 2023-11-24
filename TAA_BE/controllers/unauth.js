58de011f8427cee2be294a2a731ccd47e63bbcd7
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2023-11-24 16:17:13 +0700

    Format code controllers/unauth.js

--- TAA_BE/controllers/unauth.js
+++ TAA_BE/controllers/unauth.js
@@ -1,48 +1,33 @@
-// const express  = require('express');
-// const models = require('../models')
-// const db = require('../config/db');
-// const { error } = require('jquery');
-// const bodyParser = require("body-parser");
-// const sanitizeHtml = require('sanitize-html');
-// const md5 = require("md5");
-// const { use } = require("passport");
-
-// const app = express();
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-function unAuth() {}
+function unAuth() { }
 
 unAuth.registerPost = (req, res) => {
-    const { name , fName , phone , email, pasword , confirmPass} = req.body;
-    
-    models.unAuth.checkIfAccountExists({ name }, (err, result) => {
+  const { name, fName, phone, email, pasword, confirmPass } = req.body;
+
+  models.unAuth.checkIfAccountExists({ name }, (err, result) => {
+    if (err) throw err;
+
+    if (result) {
+      res.status(404).json({
+        msg: 'Account already exists'
+      })
+    } else {
+      const encrypPass = md5(pasword);
+      const cleanName = sanitizeHtml(name);
+      const cleanNumber = sanitizeHtml(phone);
+      const cleanEmail = sanitizeHtml(email);
+
+      models.unAuth.createAccount({ cleanName, cleanNumber, cleanEmail, encrypPass }, (err, result) => {
         if (err) throw error;
-
-        if (result) {
-            res.status(404).json({
-                msg: 'Account already exists'
-            })
-        } else {
-            const encrypPass  = md5(pasword);
-            const cleanName = sanitizeHtml(name);
-            const cleanNumber = sanitizeHtml(phone);
-            const cleanEmail = sanitizeHtml(email);
-
-            models.unAuth.createAccount({cleanName, cleanNumber, cleanEmail, encrypPass},(err,result) => {
-                if (err) throw error;
-                else {
-                    console.log("done");
-                    res.redirect("/");
-                }
-            })
-            res.status(200).json({
-                
-               
-            })
+        else {
+          console.log("done");
+          res.redirect("/");
         }
-    })
-   
+      })
+      res.status(200).json({
+
+
+      })
+    }
+  })
 }
 module.exports = unAuth
