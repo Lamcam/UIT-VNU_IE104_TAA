880fe225e8a0df777a3992e8ea90f3b75ab539fc
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2023-11-24 13:51:49 +0700

    Refactor /scripts/modules/main.js

--- TAA_FE/public/scripts/modules/main.js
+++ TAA_FE/public/scripts/modules/main.js
@@ -1,5 +1,5 @@
 import modalCtl from './modal--controller.js';
-import cookieCtl from './cookie.js';
+// import cookieCtl from './cookie.js';
 
 window.modalCtl = modalCtl;
 // window.modalCtl = cookieCtl;
@@ -11,43 +11,3 @@ function closeModal() {
   const modals = document.querySelectorAll(".modal");
   modals.forEach((modal) => modal.classList.remove("active"));
 }
-
-
-
-$(document).ready(function(){
-
-  $(document).on("click",".profile3",function(event){
-    event.preventDefault();
-
-    localStorage.setItem('flag',1)
-    window.location.replace("/TAA_FE/src/homepage/accountPage/profileInfo.html")
-    
-
-  })
-
-  $(document).on("click",".order3",function(event){
-    event.preventDefault();
-
-    localStorage.setItem('flag',2)
-    window.location.replace("/TAA_FE/src/homepage/accountPage/profileInfo.html")
-    
-
-  })
-
-  $(document).on("click",".product3",function(event){
-    event.preventDefault();
-
-    localStorage.setItem('flag',3)
-    window.location.replace("/TAA_FE/src/homepage/accountPage/profileInfo.html")
-    
-
-  })
-
-})
-
-
-
-
-
-
-
