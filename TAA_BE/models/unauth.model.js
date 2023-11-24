6c4bd8ad6dee81ec36e89c78581ebf60ad6c2c56
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2023-11-24 10:24:56 +0700

    Format code models/

--- TAA_BE/models/unauth.model.js
+++ TAA_BE/models/unauth.model.js
@@ -1,25 +1,24 @@
 const db = require('../config/db')
 
-function unAuthModel() {}
+function unAuthModel() { }
 
 unAuthModel.checkIfAccountExists = ({ name }, callback) => {
-
     const sql = 'select * from users where user_name = ?'
     const params = [name];
 
     db.query(sql, params, (err, result) => {
         callback(err, result)
     })
-} 
+}
 
-unAuthModel.createAccount = ({name, phone, email, password},callback) =>{
+unAuthModel.createAccount = ({ name, phone, email, password }, callback) => {
     const sql = 'INSERT INTO users (user_id, user_name, user_phone, user_email, user_pass, user_avatar_url, loca_default_id)\
     VALUES (?, ?, ?, ?)'
 
     const params = [name, phone, email, password];
-    
-    db.inset(sql,params,(err,result)=>{
-        callback(err,result);
+
+    db.inset(sql, params, (err, result) => {
+        callback(err, result);
     })
 
 }
