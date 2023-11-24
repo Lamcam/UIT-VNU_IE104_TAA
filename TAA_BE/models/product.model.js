3402b4dcb0de0a4c9c7ef856a5663ef12c1b4107
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2023-11-25 02:01:48 +0700

    Update model

--- TAA_BE/models/product.model.js
+++ TAA_BE/models/product.model.js
@@ -1,6 +1,6 @@
 const db = require('../config/db')
 
-function producthModel() {}
+function producthModel() { }
 
 producthModel.getAllProduct = (callback) => {
     const sql = `
@@ -10,6 +10,22 @@ producthModel.getAllProduct = (callback) => {
             ON products.prod_id = productsimg.prod_id
         INNER JOIN categories
             on products.cate_id = categories.cate_id
+    `;
+
+    db.query(sql, (err, result) => {
+        callback(err, result)
+    })
+}
+
+producthModel.getHotProduct = (callback) => {
+    const sql = `
+        SELECT *
+        FROM products
+        INNER JOIN productsimg
+            ON products.prod_id = productsimg.prod_id
+        INNER JOIN categories
+            on products.cate_id = categories.cate_id
+        WHERE prod_num_sold > 5
     `;
 
     db.query(sql, (err, result) => {
