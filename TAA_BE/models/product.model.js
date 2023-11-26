b9f2b746ec80d13effe5ddefee4e53c19db1f2dc
LamCam <lamcam813@gmail.com>
2023-11-26 17:41:04 +0700

    Fix productPage with ejs

--- TAA_BE/models/product.model.js
+++ TAA_BE/models/product.model.js
@@ -33,6 +33,20 @@ producthModel.getHotProduct = (callback) => {
     })
 }
 
+producthModel.getDetailProduct = (callback) => {
+    const sql = `
+        SELECT *
+        FROM products
+        INNER JOIN productsimg
+            ON products.prod_id = productsimg.prod_id
+        INNER JOIN categories
+            on products.cate_id = categories.cate_id;
+    `;
+
+    db.query(sql, (err, result) => {
+        callback(err, result)
+    })
+}
 
 
 
