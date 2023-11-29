a50dca793634e886af8dbfc54fe0080217007f9f
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2023-11-29 09:27:34 +0700

    Update /account/cart/add

--- TAA_FE/public/scripts/modules/cart--hander.js
+++ TAA_FE/public/scripts/modules/cart--hander.js
@@ -17,25 +17,25 @@ const addToCart = () => {
     modalCtl.closeModal()
     modalCtl.openModal('#modal--noti-add-cart-success')
 
-    // fetch('/account/cart/add', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(data),
-    // }).then(response => response.json())
-    //   .then(data => {
-    //     if (data.statusCode == 200) {
-    //       modalCtl.closeModal()
-    //       modalCtl.openModal('#modal--noti-add-cart-success')
-    //       // modalCtl.closeModalAfterTime(5000)
-    //     } else if (data.statusCode == 500) {
-    //       alert('Sản phẩm này đã tồn tại trong giỏ hàng của bạn')
-    //     }
-    //   })
-    //   .catch(error => {
-    //     console.log('Lỗi:', error);
-    //   });
+    fetch('/account/cart/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    }).then(response => response.json())
+      .then(data => {
+        if (data.statusCode == 200) {
+          modalCtl.closeModal()
+          modalCtl.openModal('#modal--noti-add-cart-success')
+          modalCtl.closeModalAfterTime(1000)
+        } else if (data.statusCode == 500) {
+          alert('Sản phẩm này đã tồn tại trong giỏ hàng của bạn')
+        }
+      })
+      .catch(error => {
+        console.log('Lỗi:', error);
+      });
   }
 }
 
