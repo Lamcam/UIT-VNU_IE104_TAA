5d5ff55fdc09556367b6de890ca72f093ed501a1
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2023-12-05 14:02:11 +0700

    Add visibility password : modal--login

--- TAA_FE/public/scripts/modules/main.js
+++ TAA_FE/public/scripts/modules/main.js
@@ -7,12 +7,14 @@ import authCtl from './auth--handler.js';
 import headerCtl from './header--controller.js';
 import cartCtl from './cart--hander.js';
 import cookieHder from './cookie--handler.js';
+import formCtl from './form--controller.js';
 // import accountHder from './account--handler.js';
 
 window.cartCtl = cartCtl;
 window.modalCtl = modalCtl;
 window.authCtl = authCtl;
 window.cookieHder = cookieHder;
+window.formCtl = formCtl;
 
 modalCtl.init();
 headerCtl.init();
